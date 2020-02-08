9295d6da8d4340ed3dc723b355e7078221959e35
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-02-08 13:42:06 +0000

    Convert document inference to python

--- inference_document_selection.py
+++ inference_document_selection.py
@@ -1,20 +1,17 @@
 import argparse
-import os
-import pickle
-import pprint
 
-import numpy as np
 import pandas as pd
+from google.colab import drive
 import torch
-from scipy.special import softmax
 from transformers import *
+import numpy as np
+import os
+import json
+import pprint
+from scipy.special import softmax
+import pickle
 
 from inference_util import create_dataloader_dev
-
-WORK_DIR = '/content/drive/My Drive/Overig'
-N = 5
-pp = pprint.PrettyPrinter(indent=4)
-OUT_TAG = 'dev_sentences_on_bert_doc_inputs'
 
 
 def unpack_batch(batch):
@@ -24,9 +21,13 @@ def unpack_batch(batch):
     y_true = batch[3]
     claim_id = batch[4]
     doc_id = [doc_ids[idx] for idx in batch[5]]  # retrieve doc ids
-    sentence_idx = [sentence_idxs[idx] for idx in batch[5]]  # retrieve sentence_idx
-    return input_ids, attention_mask, type_ids, y_true, claim_id, doc_id, sentence_idx
+    return input_ids, attention_mask, type_ids, y_true, claim_id, doc_id
 
+
+WORK_DIR = '/content/drive/My Drive/Overig'
+N = 5
+pp = pprint.PrettyPrinter(indent=4)
+OUT_TAG = 'using 50 docs from baseline'
 
 # Sorry for the bloated file, it was converted from a notebook
 if __name__ == '__main__':
@@ -34,6 +35,12 @@ if __name__ == '__main__':
     parser.add_argument("--features", required=True, type=str)
     parser.add_argument("--data", required=True, type=str)
     parser.add_argument("--model", required=True, type=str)
+    parser.add_argument("--docs", required=True, type=str)
+    parser.add_argument("--previousevidence", required=True, type=bool)
+    parser.add_argument("--firsthalf", required=True, type=bool)
+    parser.add_argument("--halfdata", required=True, type=bool)
+
+
 
     args = parser.parse_args()
 
@@ -41,21 +48,48 @@ if __name__ == '__main__':
     cached_features_file_dev = os.path.join(WORK_DIR, features_fname)  # nog maken
     data_fname = os.path.join(WORK_DIR, args.data)  # nog maken
     model_fname = args.model
+    docs_input_file = os.path.join(WORK_DIR, args.docs)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    n_gpu = torch.cuda.device_count()
-    print(torch.cuda.get_device_name(0))
+    with open(docs_input_file, "r") as in_file:
+        instances = []
+        for line in in_file:
+            instances.append(json.loads(line))
+    print(len(instances))
+    for instance in instances:
+        instance.pop('predicted_pages', None)  # drop all predicted sentences, since that's what we're doing
 
     data = pd.read_csv(data_fname)
+    data.head(10)
+
+    claim_ids = list(data.claim_id)
+    claims_not_reduced = claim_ids
+    doc_ids = list(data.doc_id)
+    del data
 
     print("Load cached dev features")
     features_dev = torch.load(cached_features_file_dev)
     print("Loaded features")
     print(f'Len features: {len(features_dev)}')
+    number_instances = 500000
 
-    claim_ids = list(data.id)
-    doc_ids = list(data.doc_id)
-    sentence_idxs = list(data.sentence_idx)
+    if args.halfdata:
+        if args.firsthalf:
+            features_dev = features_dev[:number_instances]
+            claim_ids = claim_ids[:number_instances]
+            doc_ids = doc_ids[:number_instances]
+        else:
+            features_dev = features_dev[number_instances:]
+            claim_ids = claim_ids[number_instances:]
+            doc_ids = doc_ids[number_instances:]
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    n_gpu = torch.cuda.device_count()
+    torch.cuda.get_device_name(0)
+
+    features_length = len(features_dev)
+    print(f'Len features after: {features_length}')
+    print(f'Len doc ids: {len(doc_ids)}')
+    print(f'Len claim ids: {len(claim_ids)}')
 
     torch.cuda.empty_cache()
     print("Create dev dataloader")
@@ -65,10 +99,17 @@ if __name__ == '__main__':
     model = BertForSequenceClassification.from_pretrained(f"/content/drive/My Drive/Cambridge/L101/{model_fname}",
                                                           num_labels=2)
     model.cuda()
-    pass  # suppress model.cuda output
 
-    evidence = dict((el, []) for el in dict.fromkeys(claim_ids))
-    evidence_all_scores = dict((el, []) for el in dict.fromkeys(claim_ids))
+    if args.previousevidence:
+        with open('/content/drive/My Drive/Overig/docs_evidence_full_hnm.pkl', 'rb') as f:
+            evidence = pickle.load(f)
+        with open('/content/drive/My Drive/Overig/docs_evidence_all_full_hnm.pkl', 'rb') as f:
+            evidence_all_scores = pickle.load(f)
+        print("Loaded evidence")
+    else:
+        evidence = dict((el, []) for el in dict.fromkeys(claims_not_reduced))
+        evidence_all_scores = dict((el, []) for el in dict.fromkeys(claims_not_reduced))
+    print(f"Number of claims: {len(evidence.keys())}")
     print("Done")
 
     model.eval()
@@ -87,8 +128,7 @@ if __name__ == '__main__':
         # Move batch to GPU
         batch = tuple(t.to(device) for t in batch)
         # Unpack values
-        input_ids, attention_mask, type_ids, y_true, claim_ids_batch, doc_ids_batch, sentence_idxs_batch = unpack_batch(
-            batch)
+        input_ids, attention_mask, type_ids, y_true, claim_ids_batch, doc_ids_batch = unpack_batch(batch)
         # Telling the model not to compute or store gradients, saving memory and speeding up validation. taken from https://mccormickml.com/2019/07/22/BERT-fine-tuning/
         with torch.no_grad():
             # Forward pass, calculate logit predictions
@@ -106,13 +146,16 @@ if __name__ == '__main__':
             # print(softmax_logits)
             # print(f'For claim {claim_id}; doc_id: {doc_ids_batch[i]}; sentence idx: {sentence_idxs_batch[i]}')
             # save all regression scores to dict
-            evidence_all_scores[claim_id.item()].append([softmax_logits[1], doc_ids_batch[i], sentence_idxs_batch[i]])
+            evidence_all_scores[claim_id.item()].append([softmax_logits[1], doc_ids_batch[i]])
 
             # save just the sentences labeled as evidence to dict
             if pred_flat[i] == 1:  # only if classified as evidence
-                evidence[claim_id.item()].append([softmax_logits[1], doc_ids_batch[i], sentence_idxs_batch[i]])
+                evidence[claim_id.item()].append([softmax_logits[1], doc_ids_batch[i]])
 
-    with open(f'/content/drive/My Drive/Overig/sentence_evidence_from{features_fname}.pkl', 'wb') as f:
+    mean_number_evidence_predictions_per_claim = np.mean([len(value) for value in evidence.values()])
+    print(mean_number_evidence_predictions_per_claim)
+
+    with open('/content/drive/My Drive/Overig/docs_evidence_full_hnm.pkl', 'wb') as f:
         pickle.dump(evidence, f)
-    with open(f'/content/drive/My Drive/Overig/sentence_evidence_all_from{features_fname}.pkl', 'wb') as f:
+    with open('/content/drive/My Drive/Overig/docs_evidence_all_full_hnm.pkl', 'wb') as f:
         pickle.dump(evidence_all_scores, f)
