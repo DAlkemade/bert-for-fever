40cd9f388e63ff8ea852d72f6d931b4a9c304051
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2020-03-11 21:16:40 +0000

    Refactor main function

--- train_bert_classifier.py
+++ train_bert_classifier.py
@@ -43,90 +43,97 @@ def create_dataloader_training(features):
     return dataloader
 
 
-# Sorry for the bloated file, it was converted from a notebook
-if __name__ == '__main__':
+class BertModel(object):
+    def __init__(self):
+        self.model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+        self.model.cuda()
+        # This is a very standard piece of code for the optimizer parameters, available from many sources
+        # e.g. https://worksheets.codalab.org/rest/bundles/0x60ed20fc419641d799f53aa0667d5713/contents/blob/basic_trainer.py
+        param_optimizer = list(self.model.named_parameters())
+        no_decay = ['bias', 'gamma', 'beta']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+             'weight_decay_rate': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+             'weight_decay_rate': 0.0}
+        ]
+        self.optimizer = AdamW(optimizer_grouped_parameters,
+                               lr=2e-5)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def train(self, dataloader_train):
+        train_loss_set = []
+
+        for _ in trange(EPOCHS, desc="Epoch"):
+
+            # TRAINING ON TRAIN SET
+            self.model.train()
+
+            # Train the data for one epoch
+            for step, batch in enumerate(dataloader_train):
+                if step % 1000 == 0:
+                    print(f'\nAt step {step}')
+                # Move batch to GPU
+                batch = tuple(t.to(self.device) for t in batch)
+                # Unpack values
+                input_ids, attention_mask, type_ids, y_true = unpack_batch(batch)
+                # Clear out the gradients (by default they accumulate) taken from https://mccormickml.com/2019/07/22/BERT-fine-tuning/
+                self.optimizer.zero_grad()
+                # Make predictions for the batch
+                outputs = self.model(input_ids, token_type_ids=type_ids, attention_mask=attention_mask, labels=y_true)
+                loss = outputs[0]
+                train_loss_set.append(loss.item())
+                loss.backward()  # Backpropagate
+                self.optimizer.step()
+
+        return train_loss_set
+
+    def save(self):
+        self.model.save_pretrained(WORK_DIR)
+
+
+def sample_negative(features_train):
+    new_features = []
+    for f in features_train:
+        if f.label == 1:
+            new_features.append(f)
+        else:
+            # throw away 60% of neg instances at random
+            if random.uniform(0, 1) < 0.5:
+                new_features.append(f)
+    return new_features
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--features', required=True)
     parser.add_argument('--samplenegative', default=True)
 
     args = parser.parse_args()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    n_gpu = torch.cuda.device_count()
     torch.cuda.get_device_name(0)
-
     print("Load cached training features")
     cached_features_file_train = os.path.join(WORK_DIR, args.features)
-    print(cached_features_file_train)
     features_train = torch.load(cached_features_file_train)
-    len(features_train)
-
-    np.bincount(torch.tensor([f.label for f in features_train], dtype=torch.long))
-
     if args.samplenegative:
-        new_features = []
-        for f in features_train:
-            if f.label == 1:
-                new_features.append(f)
-            else:
-                # throw away 60% of neg instances at random
-                if random.uniform(0, 1) < 0.5:
-                    new_features.append(f)
-        features_train = new_features
-
-    len(features_train)
+        features_train = sample_negative(features_train)
 
     torch.cuda.empty_cache()
+
     print("Create train dataloader")
     dataloader_train = create_dataloader_training(features_train)
     del features_train
     print(f"Len train dataloader: {len(dataloader_train)}")
 
-    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
-    model.cuda()
-    pass  # suppress model.cuda output
-
-    # This is a very standard piece of code for the optimizer parameters, available from many sources, e.g. https://worksheets.codalab.org/rest/bundles/0x60ed20fc419641d799f53aa0667d5713/contents/blob/basic_trainer.py
-    param_optimizer = list(model.named_parameters())
-    no_decay = ['bias', 'gamma', 'beta']
-    optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-         'weight_decay_rate': 0.01},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-         'weight_decay_rate': 0.0}
-    ]
-
-    optimizer = AdamW(optimizer_grouped_parameters,
-                      lr=2e-5)
-
-    train_loss_set = []
-
-    # use trange for a prediction of the time it will take and to record execution times
-    for _ in trange(EPOCHS, desc="Epoch"):
-
-        # TRAINING ON TRAIN SET
-        model.train()
-
-        # Train the data for one epoch
-        for step, batch in enumerate(dataloader_train):
-            if step % 1000 == 0:
-                print(f'\nAt step {step}')
-            # Move batch to GPU
-            batch = tuple(t.to(device) for t in batch)
-            # Unpack values
-            input_ids, attention_mask, type_ids, y_true = unpack_batch(batch)
-            # Clear out the gradients (by default they accumulate) taken from https://mccormickml.com/2019/07/22/BERT-fine-tuning/
-            optimizer.zero_grad()
-            # Make predictions for the batch
-            outputs = model(input_ids, token_type_ids=type_ids, attention_mask=attention_mask, labels=y_true)
-            loss = outputs[0]
-            train_loss_set.append(loss.item())
-            loss.backward()  # Backpropagate
-            optimizer.step()
-
-    model.save_pretrained(WORK_DIR)
-    print("Done!")
+    bert_model = BertModel()
+    train_loss_set = bert_model.train(dataloader_train)
+    bert_model.save()
 
     with open(os.path.join(WORK_DIR, "losses.txt"), "w") as f:
         for loss in train_loss_set:
             f.write(f'{loss}\n')
+
+
+# Sorry for the bloated file, it was converted from a notebook
+if __name__ == '__main__':
+    main()
